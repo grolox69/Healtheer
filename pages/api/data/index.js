70024feb69d6828db6ca38fd8d0405130e161d15
grolox69 <karl.gharios@gmail.com>
2022-04-09 20:27:35 +0300

    Fix /data route

--- pages/api/data/index.js
+++ pages/api/data/index.js
@@ -1,9 +1,15 @@
 import { createHandler } from "../../../util/ConnectHandler/createHandler";
 
+// @route /api/data
+// @desc GET arduino's data
+// @access Private
+
 const handler = createHandler();
 
 handler
 .post((req, res) => {
-    console.log(req);
+    console.log(req.body);
     res.json({ msg: 'Felefly byeklo'})
 })
+
+export default handler;
